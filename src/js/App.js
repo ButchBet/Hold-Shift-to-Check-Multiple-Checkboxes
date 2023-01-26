6bde4f302156824502eced3786a3cbb3391d23bd
ButchBet <butchbet01@gmail.com>
2023-01-25 22:58:30 -0500

    Created function to know the range of the selection or deselection.

--- src/js/App.js
+++ src/js/App.js
@@ -1,4 +1,38 @@
+// Saving the checkboxes and texts
+const checkboxes = Array.from(document.getElementsByClassName("list__checkbox"));
+const texts = Array.from(document.getElementsByClassName("list__text"));
 
-const checkboxes = document.getElementsByClassName("list__checkbox");
-const texts = document.getElementsByClassName("list__text");
+// Array to save objects with the start and end points
+const tour = []; // {start: index1, end: index2}
 
+// Add click event to each checkbox
+checkboxes.forEach((checkbox, index) => {
+    checkbox.addEventListener("click", (e) => {
+        let [stopPoint, address] = [null, null];
+        [stopPoint, address] = findAddress(index);
+        console.log(stopPoint, address)
+        texts[index].classList.toggle("lineThrough");
+
+    })
+})
+
+// Function to know if the we have to select the boxes from top to down, reverser or don't slect them
+function findAddress(index) {
+    let stopPoint = index + 1;
+    console.log(stopPoint);
+    for(stopPoint; stopPoint < checkboxes.length; stopPoint++) {
+        if(texts[stopPoint].classList.contains("lineThrough")) {
+            return [stopPoint, true];
+        }
+    }
+
+    stopPoint = index - 1;
+    console.log(stopPoint);
+    for(stopPoint; stopPoint >= 0; stopPoint--) {
+        if(texts[stopPoint].classList.contains("lineThrough")) {
+            return [stopPoint, false];
+        }
+    }
+
+    return [stopPoint, null];
+}
