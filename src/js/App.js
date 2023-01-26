d8fa51ffc12d9051d97a9a7e684ec4474b47a212
ButchBet <butchbet01@gmail.com>
2023-01-26 16:52:37 -0500

    Created and implemented the checkboxesUnselect function. Also I have updated the structure to select a range of boxes

--- src/js/App.js
+++ src/js/App.js
@@ -3,61 +3,96 @@ const checkboxes = Array.from(document.getElementsByClassName("list__checkbox"))
 const texts = Array.from(document.getElementsByClassName("list__text"));
 
 // Array to save arrays with the start and end points
-const tours = []; // [[start: index1, end: index2]]
+let tours = []; // [[start: index1, end: index2]]
 
 // Add click event to each checkbox
 checkboxes.forEach((checkbox, index) => {
-    checkbox.addEventListener("click", (e) => {
-        let [stopPoint, address] = [null, null];
-        
-        if(!checkbox.classList.contains("lineThrough")) {
-            [stopPoint, address] = findAddress(index);
-
-
-            let tour = [index, stopPoint];
-            tour.sort((a, b) => a - b);
-
-            console.log(tour);
-            
-            checkboxesSelect(tour);
-            
-            tours.push(tour);
+    checkbox.addEventListener("click", (e) => {        
+        if(!texts[index].classList.contains("lineThrough")) {
+            findAddress(index);
         } else {
-
+            checkboxesUnselect(index);
+            texts[index].classList.remove("lineThrough");
         }
-        texts[index].classList.toggle("lineThrough");
+
+        // texts[index].classList.toggle("lineThrough");
         
+        tours.so
         console.log(tours);
     })
 })
 
 // Function to know if the we have to select the boxes from top to down, reverser or don't slect them
-function findAddress(index) {
-    let stopPoint = index + 1;
-    console.log(stopPoint);
-    for(stopPoint; stopPoint < checkboxes.length; stopPoint++) {
-        if(texts[stopPoint].classList.contains("lineThrough")) {
-            return [stopPoint, true];
-        }
-    }
+function findAddress(indexSelected) {
+    const checkboxesClone = checkboxes;
+    let tour = [null, null];
 
-    stopPoint = index - 1;
-    console.log(stopPoint);
-    for(stopPoint; stopPoint >= 0; stopPoint--) {
-        if(texts[stopPoint].classList.contains("lineThrough")) {
-            return [stopPoint, false];
-        }
-    }
+    for(let i = 0; i < checkboxesClone.length; i++) {
+        if(checkboxesClone[i].checked === true && indexSelected > i) {
+            console.log("first")
 
-    return [stopPoint, null];
+            // for(let j = 0; j < tours.length; j++) {
+            //     if(tours[j][1] === i) {
+            //         tour = [i, indexSelected];
+                    
+            //         checkboxesSelect(tour);
+                    
+            //         tours.push(tour);
+
+            //         i = indexSelected - 1;
+            //     }
+            // }
+
+            tour = [i, indexSelected];
+            
+            checkboxesSelect(tour);
+            
+            tours.push(tour);
+
+            i = indexSelected - 1;
+        } else if(checkboxesClone[i].checked === true && indexSelected < i ) {
+            console.log("second");
+            
+            tour = [indexSelected, i];
+            
+            checkboxesSelect(tour);
+            
+            tours.push(tour);
+        } else if (i === indexSelected) {
+            console.log("third")
+
+            texts[indexSelected].classList.add("lineThrough");
+        } 
+    }
 }
 
 
 // Function to select the respective checkboxes and line-through the respective texts
 function checkboxesSelect(tour) {
-    for(let index = tour[0] + 1; index < tour[1]; index++) {
-        checkboxes[index].checked = true;
+    for(let i = tour[0] + 1; i < tour[1]; i++) {
+        console.log(i);
+        checkboxes[i].checked = true;
 
-        texts[index].classList.add("lineThrough");
+        texts[i].classList.add("lineThrough");
     }
+}
+
+// Function to unselect the respective checkboxes and line-through the respective texts
+function checkboxesUnselect(indexUnselect) {
+    const newTours = [];
+    console.log(`Unchecked ${indexUnselect}`);
+
+    tours.forEach((tour, index) => {
+        if(tour[0] === indexUnselect || tour[1] === indexUnselect) {
+            for(let i = tour[0] + 1; i < tour[1]; i++) {
+                checkboxes[i].checked = false;
+
+                texts[i].classList.remove("lineThrough");
+            }
+        } else {
+            newTours.push(tour);
+        }
+    });
+
+    tours = newTours;
 }
